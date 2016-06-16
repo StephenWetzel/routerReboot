82f60ec4f5089143b458652418e83c5eab728a5e
Stephen Wetzel <stephen.wetzel1@gmail.com>
2016-06-16 15:22:44 -0400

    Update to check router status with wget, instead of just ping, also treat router up but internet down differently than router down

--- checkInternet.py
+++ checkInternet.py
@@ -1,23 +1,39 @@
 import os
 
 routerIp = "192.168.2.1" #your router's IP
-sites = ["8.8.8.8", "google.com", "facebook.com"] #a list of sites to try
+sites = ["google.com", "facebook.com", "s3.amazonaws.com"] #a list of sites to try
 
 numPingsRouter = "1" #if a single ping fails to your own router then something is wrong
 numPingsSites = "3"
+intervalSites = "1" #seconds between pings to sites
+
+#here we ping site to see if internet is up
+#there are 3 return states, up means we could ping at least one internet site
+#down means we could not ping the router, we short circuit in this case as router needs to be restarted immediately
+#router means we could ping router, but not internet sites, we allow calling function decide how to handle this
 
 def checkUp():
+	#can we ping router?
 	response = os.system("ping -c " + numPingsRouter + " " + routerIp + " 2>&1 >/dev/null")
-	#print response
 	if response != 0:
 		#if we can't even ping router then we are definitely down
-		return False
+		return "down"
 
+	#does router serve status page?
+	response = os.system("wget -qO- " + routerIp + " 2>&1 >/dev/null")
+	if response != 0:
+		return "down"
+
+	#try internet sites
 	for site in sites:
-		response = os.system("ping -c " + numPingsSites + " " + site + " 2>&1 >/dev/null")
+		response = os.system("ping -c " + numPingsSites + " -i " + intervalSites + " " + site + " 2>&1 >/dev/null")
 		if response == 0:
 			#if we successfully ping any internet site then we are definitely up
-			return True
+			return "up"
 
-	#if we got here we couldn't ping any site
-	return False
+	#if we got here we couldn't ping any site, but could ping router
+	return "router"
+
+
+if __name__ == '__main__':
+	print checkUp()
